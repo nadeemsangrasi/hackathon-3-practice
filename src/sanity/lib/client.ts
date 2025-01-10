7f30358a02a4999ef48dc1a50833d973ac61dc4a
nadeem <nadeemsangrasi903@gmail.com>
2025-01-11 00:39:21 +0500

    change

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // Replace with your project ID
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // Or your dataset name
+  projectId: process.env.PROJECT_ID! || "", // Replace with your project ID
+  dataset: process.env.DATASET! || "", // Or your dataset name
   apiVersion: "2024-01-04", // Today's date or latest API version
   useCdn: false, // Disable CDN for real-time updates
   token: process.env.token,
